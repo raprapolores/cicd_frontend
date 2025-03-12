4c9cb94203a3605b9bc8f1dcdbc1d9b834e940f8
20242174-RALPH\admin <oloresralph@gmail.com>
2025-03-13 11:46:26 +1300

    Finished login and view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./Components/Home";
 import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Logout from "./Components/Logout";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
           {/*<Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
           <Route path="contact" element={<Contact />} />
